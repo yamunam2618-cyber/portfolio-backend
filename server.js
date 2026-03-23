fcbf8e0cbf5bca8f85a4ebee158a668861b12b02
Yamuna M <yamuna.m2618@gmail.comgit config --global user.name Yamuna>
2026-03-24 00:23:46 +0530

    fixed syntax error finally

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.post("/feedback", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
